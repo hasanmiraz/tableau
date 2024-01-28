23a33363f40551fc55acd99039805c8601b28c96
hasibul-miraz <hasibul.miraz@gmail.com>
2024-01-28 22:41:49 +1100

    BlogMe data cleaning and creating complete

--- blog_me/blog_me.py
+++ blog_me/blog_me.py
@@ -6,6 +6,7 @@ Created on Sun Jan 28 18:46:43 2024
 """
 
 import pandas as pd
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 # read files
 data = pd.read_excel('data/articles.xlsx')
@@ -18,3 +19,64 @@ data.groupby(['source_id'])['article_id'].count() # counting articales from uniq
 data.groupby(['source_id'])['engagement_reaction_count'].sum() # counting total likes from unique sources
 data.groupby(['source_id'])['engagement_comment_count'].sum() # counting total comments from unique sources
 data.groupby(['source_id'])['engagement_share_count'].sum() # counting total shares from unique sources
+
+# dropping column
+data = data.drop('engagement_comment_plugin_count', axis = 1)
+
+# keyword flagging
+
+keywords = ['crash', 'murder', 'attack']
+keyword_flag = []
+for title in data['title']:
+    try:
+        if len([word for word in keywords if word in title])>0 :
+            keyword_flag.append(1)
+        else:
+            keyword_flag.append(0)
+    except:
+        keyword_flag.append(0)
+data['keyword_flag'] = pd.Series(keyword_flag)
+
+# sentiment analyzing
+sent_analyzer = SentimentIntensityAnalyzer()
+
+title_positive_sentiment = []
+title_negative_sentiment = []
+title_neutral_sentiment = []
+
+for title in data['title']:
+    try:
+        sentiment = sent_analyzer.polarity_scores(title)
+        title_positive_sentiment.append(sentiment['pos'])
+        title_negative_sentiment.append(sentiment['neg'])
+        title_neutral_sentiment.append(sentiment['neu'])
+    except:
+        title_positive_sentiment.append(0)
+        title_negative_sentiment.append(0)
+        title_neutral_sentiment.append(0)
+data['title_positive_sentiment'] = pd.Series(title_positive_sentiment)
+data['title_negative_sentiment'] = pd.Series(title_negative_sentiment)
+data['title_neutral_sentiment'] = pd.Series(title_neutral_sentiment)
+
+# saving file as xlsx
+data.to_excel('data/blog_me_final.xlsx', sheet_name = "BlogMe", index = False)
+data.to_csv('data/blog_me_final.csv', index = False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
